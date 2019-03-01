bb4b61c2b6eb7e6e9c9c1208769d8163cc3d48c8
nixswinner <nixondeveloper@gmail.com>
2019-03-01 23:39:41 +0300

    fix UI add two water tanks

--- app/src/main/java/com/nix/watermonitor/MainActivity.java
+++ app/src/main/java/com/nix/watermonitor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nix.watermonitor;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,9 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.github.lzyzsd.circleprogress.CircleProgress;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TextView mTextMessage,levelA,levelB;
+    private CircleProgress circle_progressA,circle_progressB;
+    private int waterLevelA=0,waterLevelB=0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,6 +42,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        levelA = (TextView) findViewById(R.id.levelA);
+        levelB = (TextView) findViewById(R.id.levelB);
+        circle_progressA = findViewById(R.id.circle_progressA);
+        circle_progressB = findViewById(R.id.circle_progressB);
+        waterLevelA=90;
+        waterLevelB = 29;
+        levelA.setText(waterLevelA+"%");
+        levelB.setText(""+waterLevelB+"%");
+        circle_progressA.setProgress(waterLevelA);
+        if (waterLevelA<30){
+            circle_progressA.setFinishedColor(Color.RED);
+        }
+        circle_progressB.setProgress(waterLevelB);
+        if (waterLevelB<30){
+            circle_progressB.setFinishedColor(Color.RED);
+        }
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
