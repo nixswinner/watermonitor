b4ed5395a60d8cdecc01a9efb05a398620173cc9
nixswinner <nixondeveloper@gmail.com>
2019-03-18 22:21:44 +0300

    navmenu changed

--- app/src/main/java/com/nix/watermonitor/MainActivity.java
+++ app/src/main/java/com/nix/watermonitor/MainActivity.java
@@ -11,10 +11,9 @@ import android.widget.TextView;
 import com.github.lzyzsd.circleprogress.CircleProgress;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage,levelA,levelB;
-    private CircleProgress circle_progressA,circle_progressB;
-    private int waterLevelA=0,waterLevelB=0;
+    private TextView mTextMessage,levelA,levelB,levelD,levelC;
+    private CircleProgress circle_progressA,circle_progressB,circle_progressC,circle_progressD;
+    private int waterLevelA=0,waterLevelB=0,waterLevelC=0,waterLevelD=0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -23,10 +22,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("My Water Tanks Levels");
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -44,19 +40,44 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         levelA = (TextView) findViewById(R.id.levelA);
         levelB = (TextView) findViewById(R.id.levelB);
+        levelC = (TextView) findViewById(R.id.levelC);
+        levelD = (TextView) findViewById(R.id.levelD);
+
         circle_progressA = findViewById(R.id.circle_progressA);
         circle_progressB = findViewById(R.id.circle_progressB);
+        circle_progressC = findViewById(R.id.circle_progressC);
+        circle_progressD = findViewById(R.id.circle_progressD);
         waterLevelA=90;
         waterLevelB = 29;
+        waterLevelC = 50;
+        waterLevelD = 16;
         levelA.setText(waterLevelA+"%");
         levelB.setText(""+waterLevelB+"%");
+        levelC.setText(""+waterLevelC+"%");
+        levelD.setText(""+waterLevelD+"%");
         circle_progressA.setProgress(waterLevelA);
         if (waterLevelA<30){
             circle_progressA.setFinishedColor(Color.RED);
+            levelA.setTextColor(Color.RED);
         }
+
         circle_progressB.setProgress(waterLevelB);
         if (waterLevelB<30){
             circle_progressB.setFinishedColor(Color.RED);
+            levelB.setTextColor(Color.RED);
+        }
+
+        circle_progressC.setProgress(waterLevelC);
+        if (waterLevelC<30){
+            circle_progressC.setFinishedColor(Color.RED);
+            levelC.setTextColor(Color.RED);
+        }
+
+        circle_progressD.setProgress(waterLevelD);
+        if (waterLevelD<30){
+            circle_progressD.setFinishedColor(Color.RED);
+            levelD.setTextColor(Color.RED);
+
         }
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
